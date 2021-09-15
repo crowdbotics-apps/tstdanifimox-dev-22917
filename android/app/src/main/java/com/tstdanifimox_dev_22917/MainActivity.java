aafa12e3a7272d46f1a4b988d7a9149e68bc1458
Crowdbotics <team@crowdbotics.com>
2021-09-15 21:51:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tstdanifimox_dev_22917/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tstdanifimox_dev_22917;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tstdanifimox_dev_22917";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tstdanifimox_dev_22917Identifier",
+  url: "https://tstdanifimox_dev_22917Identifier.botics.co",
+  api: "https://tstdanifimox_dev_22917Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
